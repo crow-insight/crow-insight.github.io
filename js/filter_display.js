7a261b4eb79b7b801c6d893408f75bedb90b4dd2
crowinsight <camden@crowinsight.com>
2020-11-03 18:26:56 -0500

    consolidated filter display functions

---
+++ js/filter_display.js
@@ -0,0 +1,210 @@
+function getTags(d) {
+	tag_string = "";
+	for (let key in d) {
+		if (+d[key] == 1) {
+
+			for (let i = 0; i < metadata.length; i++) {
+				if (key == metadata[i].Tag) {
+					if (tag_string == "") {
+						tag_string = metadata[i].PlainLanguage;
+					}
+					else {
+						tag_string = tag_string + ", " + metadata[i].PlainLanguage;
+					}
+				}
+			}
+		}
+
+	}
+	return tag_string;
+}
+
+function createBlocks(rows) {
+	let display = d3.select("#display-container")
+		.style("opacity", 0);
+
+	display.selectAll(".block-row").remove();
+
+	let row_containers = display.selectAll(".block-row")
+		.data(rows)
+		.enter()
+		.append("div")
+		.attr("class", "row block-row")
+		.html(function(d) {
+			let html_string = ""
+			for (let i = 0; i < d.length; i ++) {
+				html_string = html_string + "<div class='"+ block_column_class +" columns block'>" +
+							"<img class='u-max-full-width' src='/assets/img/modernreport.png' >" +
+							"<p><strong>" + d[i][attribute_name] + "</strong>" + "<br>" + 
+							getTags(d[i]) + "</p>" +
+							"</div>";
+			}
+
+			return html_string;
+		});
+
+	display.transition().duration(500)
+		.style("opacity", 1);
+}
+
+function createGlobals() {
+	d3.csv(lookup_file_path, function(data, i) {
+		metadata.push(data);
+		metadata_dict[data.PlainLanguage] = data.Tag;
+	})
+}
+
+function createDisplay(filters) {
+
+	rows = [];
+	display_infos = [];
+	temp_metadata_dict = {}
+
+
+	d3.csv(lookup_file_path, function(data, i) {
+		temp_metadata_dict[data.PlainLanguage] = data.Tag;
+	})
+	.then(function(d) {
+		d3.csv(tags_file_path, function(data, i) {
+			display_infos.push(data);
+		})
+		.then(function(d) {
+
+			if (filters.length > 0) {
+				display_infos = display_infos.filter(function(d) {
+					include_filter = false;
+					for (let i = 0; i < filters.length; i++) {
+						if (d[ temp_metadata_dict[filters[i]] ] == "1") {
+							include_filter = true;
+						}
+					}
+					return include_filter;
+				});
+			}
+
+			for (let i = 0; i < display_infos.length; i++) {
+
+				if (i % blocks_per_row == 0) {
+					rows.push([display_infos[i]]);
+				}
+				else {
+					let j = i;
+					while (j % blocks_per_row != 0) {
+						j = j - 1;
+					}
+					rows[j/blocks_per_row].push(display_infos[i]);
+				}
+			}
+
+			createBlocks(rows);
+		})
+	})
+}
+
+function createDrawer() {
+
+	d3.selectAll(".filter-button-wrapper button")
+		.on("click", function(d) {
+			let button_text = $(this).text();
+			let drawer_is_open = (d3.select("#filters-container").style("opacity") == 1)
+
+			if ((button_text != curr_filter_category && button_text != "All") || (button_text == curr_filter_category && drawer_is_open == false && button_text != "All") ) {
+
+				let filters = d3.select("#filters-container").style("opacity", 0);
+				d3.selectAll(".filter-row").remove();
+				curr_filter_category = button_text;
+
+				let available_filters = metadata.filter(function(d) {
+					if (button_text == "All") {
+						return true;
+					}
+					else {
+						return button_text == d.Category;
+					}
+				});
+
+				let metadata_nested = [];
+				for (let i = 0; i < available_filters.length; i++) {
+					if (i % blocks_per_row == 0) {
+						metadata_nested.push([available_filters[i]]);
+					}
+					else {
+						let j = i;
+						while (j % blocks_per_row != 0) {
+							j = j - 1;
+						}
+						metadata_nested[j/blocks_per_row].push(available_filters[i]);
+					}
+				}
+
+				let filter_rows = filters.selectAll(".filter-row")
+					.data(metadata_nested)
+					.enter()
+					.append("div")
+					.attr("class", "row filter-row")
+					.html(function(d) {
+
+						let html_string = ""
+						for (let i = 0; i < d.length; i ++) {
+							let selected_style = " style='font-weight:700;color:black;' "
+
+							if (!curr_filters.includes(d[i].PlainLanguage)) {
+								selected_style = ""
+							}
+
+							html_string = html_string + "<div class='"+ filter_column_class +" columns filter'>" +
+							"<button" + selected_style + ">" + d[i].PlainLanguage + "</button>" +
+							"</div>";
+						}
+
+						return html_string;
+					});
+
+				filters.selectAll(".filter button")
+					.on("click", function(d) {
+
+						let button_text = $(this).text();
+
+
+						if (curr_filters.includes(button_text)) {
+
+							let index = curr_filters.indexOf(button_text);
+						    if (index > -1) {
+						       curr_filters.splice(index, 1);
+						    }
+
+						    d3.select(this).style("font-weight", 400).style("color", default_font_color);
+						}
+						else {
+							curr_filters.push(button_text);
+							d3.select(this).style("font-weight", 700).style("color", "black");
+						}
+						createDisplay(curr_filters);
+
+					});
+
+				filters.transition()
+					.duration(500)
+					.style("opacity", 1);
+			}
+			else if (button_text == "All" && button_text != curr_filter_category) {
+
+				let filters = d3.select("#filters-container").style("opacity", 0);
+				d3.selectAll(".filter-row").remove();
+				curr_filter_category = button_text
+
+				curr_filters = []
+				createDisplay(curr_filters);
+			}
+			else {
+				let filters = d3.select("#filters-container").transition().duration(250).style("opacity", 0);
+				d3.selectAll(".filter-row").transition().duration(250).remove();
+				curr_filter_category = button_text;
+			}
+		});
+}
+
+
+createGlobals();
+createDisplay(curr_filters);
+createDrawer();
--- js/portfolio.js
+++ js/portfolio.js
@@ -1,73 +1,19 @@
-rows = []
-metadata = []
-blocks_per_row = 2
-column_class = "six"
+metadata = [];
+metadata_dict = {};
+
+blocks_per_row = 2;
+filters_per_row = 4;
+
+block_column_class = "six";
+filter_column_class = "six";
+
+curr_filter_category = "All";
+curr_filters = []
+
+default_font_color = "#646468";
+
+const lookup_file_path = "../assets/data/portfolio_lookup.csv";
+const tags_file_path = "../assets/data/portfolio_tags.csv";
+const attribute_name = "portfolio_item";
 
 
-function getTags(d) {
-	tag_string = "";
-	for (let key in d) {
-		if (+d[key] == 1) {
-
-			for (let i = 0; i < metadata.length; i++) {
-				if (key == metadata[i].Tag) {
-					if (tag_string == "") {
-						tag_string = metadata[i].PlainLanguage;
-					}
-					else {
-						tag_string = tag_string + ", " + metadata[i].PlainLanguage;
-					}
-				}
-			}
-		}
-
-	}
-	return tag_string;
-}
-
-function createBlocks() {
-	let portfolio = d3.select("#portfolio-container");
-
-	let row_containers = portfolio.selectAll("row")
-		.data(rows)
-		.enter()
-		.append("div")
-		.attr("class", "row item-row")
-		.html(function(d) {
-			let html_string = ""
-			for (let i = 0; i < d.length; i ++) {
-				html_string = html_string + "<div class='"+ column_class +" columns item'>" +
-							"<img class='u-max-full-width' src='/assets/img/modernreport.png' >" +
-							"<p><strong>" + d[i].portfolio_item + "</strong>" + "<br>" + 
-							getTags(d[i]) + "</p>" +
-							"</div>";
-			}
-
-			return html_string;
-		});
-}
-
-function createPortfolio() {
-	d3.csv("../assets/data/portfolio_tags.csv", function(data, i) {
-
-		if (i % blocks_per_row == 0) {
-			rows.push([data]);
-		}
-		else {
-			while (i % blocks_per_row != 0) {
-				i = i - 1;
-			}
-			rows[i/blocks_per_row].push(data);
-		}
-	})
-	.then(function(d) {
-		d3.csv("../assets/data/portfolio_lookup.csv", function(data, i) {
-			metadata.push(data);
-		})
-		.then(function(d) {
-			createBlocks();
-		})
-	})
-}
-
-createPortfolio();
